4f8345d9debc8c903481c638697a7b7f969fb3c7
Mohammad Baghban <37972709+mohammadbaghban@users.noreply.github.com>
2019-08-28 22:46:52 +0430

    Update MessengerServers.java

--- GettingStarted/MessengerServers.java
+++ GettingStarted/MessengerServers.java
@@ -1,9 +1,9 @@
 public class MessengerServers {
-  public static void main(String[] args){
-    double file1 = 12.41, file2 = 8.95, file3 = 25.88,
-    file4 = 1.58, file5 = 19.60;
-    double average = (file1 + file2 + file3
-     + file4 + file5) / 5;
-    System.out.println(average);
-  }
+public static void main(String[] args){
+double file1 = 12.41, file2 = 8.95, file3 = 25.88,
+file4 = 1.58, file5 = 19.60;
+double average = (file1 + file2 + file3
++ file4 + file5) / 5;
+System.out.println(average);
+}
 }
