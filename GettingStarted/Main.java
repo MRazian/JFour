d6fe2eb6e7e255b7be6785648e8cde1e07a1ad45
Mohammad Baghban <37972709+mohammadbaghban@users.noreply.github.com>
2019-08-28 22:40:26 +0430

    Update Main.java

--- GettingStarted/Main.java
+++ GettingStarted/Main.java
@@ -1,9 +1,9 @@
 public class Main {
- public static void main(String[] args){
- int availableGasoline = 35;
- int batteryPercentage = 68;
- int availableDistance = availableGasoline * 15 +
- batteryPercentage * 10;
- System.out.println(availableDistance);
- }
+ 	public static void main(String[] args){
+ 		int availableGasoline = 35;
+	 	int batteryPercentage = 68;
+	 	int availableDistance = availableGasoline * 15 +
+	 	batteryPercentage * 10;
+ 		System.out.println(availableDistance);
+	}
 }
