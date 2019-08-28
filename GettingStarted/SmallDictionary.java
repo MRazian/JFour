cc95c2e274b70ba8b8abd219cbc3ffa09c8e5ca3
Mohammad Baghban <37972709+mohammadbaghban@users.noreply.github.com>
2019-08-28 22:58:48 +0430

    Update SmallDictionary.java

--- GettingStarted/SmallDictionary.java
+++ GettingStarted/SmallDictionary.java
@@ -1,36 +1,35 @@
 String word = "programming";
 switch (word){
  case "future":
- System.out.println("آینده;("
+ System.out.println("آینده");
  break;
  case "mobile":
- System.out.println("موبایل;("
+ System.out.println("موبایل");
  break;
  case "here":
- System.out.println("اینجا;("
+ System.out.println("اینجا");
  break;
  case "key":
-27
- System.out.println("کلید;("
+ System.out.println("کلید");
  break;
  case "pen":
- System.out.println("خودکار;("
+ System.out.println("خودکار");
  break;
  case "bag":
- System.out.println("کیف;("
+ System.out.println("کیف");
  break;
  case "university":
- System.out.println("دانشگاه;("
+ System.out.println("دانشگاه");
  break;
  case "book":
- System.out.println("کتاب;("
+ System.out.println("کتاب");
  break;
  case "programming":
- System.out.println("برنامهنویسی;("
+ System.out.println("برنامهنویسی");
  break;
  case "development":
- System.out.println("توسعه;("
+ System.out.println("توسعه");
  break;
  default:
-;("کلمه ناشناس است.")println.out.System
+ System.out.println("کلمه ناشناس است.");
 }
