ba71fcaab905e7a0fff64d0e0eaf00429b7b15e3
Mohammad Baghban <37972709+mohammadbaghban@users.noreply.github.com>
2019-08-28 22:56:34 +0430

    Create SmallDictionary.java

---
+++ GettingStarted/SmallDictionary.java
@@ -0,0 +1,36 @@
+String word = "programming";
+switch (word){
+ case "future":
+ System.out.println("آینده;("
+ break;
+ case "mobile":
+ System.out.println("موبایل;("
+ break;
+ case "here":
+ System.out.println("اینجا;("
+ break;
+ case "key":
+27
+ System.out.println("کلید;("
+ break;
+ case "pen":
+ System.out.println("خودکار;("
+ break;
+ case "bag":
+ System.out.println("کیف;("
+ break;
+ case "university":
+ System.out.println("دانشگاه;("
+ break;
+ case "book":
+ System.out.println("کتاب;("
+ break;
+ case "programming":
+ System.out.println("برنامهنویسی;("
+ break;
+ case "development":
+ System.out.println("توسعه;("
+ break;
+ default:
+;("کلمه ناشناس است.")println.out.System
+}
